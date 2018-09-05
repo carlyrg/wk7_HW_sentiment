fa5c6aafe667eea262550655a4cb1191e263b3e7
carlyrg <36780379+carlyrg@users.noreply.github.com>
2018-09-05 15:40:37 -0600

    Create config.py

---
+++ config.py
@@ -0,0 +1,4 @@
+consumer_key='O0lIqXHCu0YRQwN0gmHMsXQHy'
+consumer_secret='Sf8cawo0rB8flAUI2oYHon0kLoaWnBnH1S6my3r5AIcMOzi7hn'
+app_key='1008873804733374464-Eqc2CpyeFEpl7nV1EgTlH4lPPhKvj7'
+app_secret='8MNfd21s8l7oC9Wvi2jIHRTJUgMZm4afLgbQ1yXCMK9AM'
